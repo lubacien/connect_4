dce7a89d1b1607c3fa115fb7fc908f2c4f563652
Lucien-Barret <lucien.barret@telepathy.ai>
2022-11-17 16:20:15 +0100

    heroku deployment

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import asyncio
 import websockets
+import os
+import signal
 #async def handler(websocket):
     # while True:
     #     try:
@@ -123,8 +125,19 @@ async def play(websocket, game, player, connected):
 
 
 async def main():
-    async with websockets.serve(handler, "", 8001):
-        await asyncio.Future()  # run forever
+    #local loop:
+    #async with websockets.serve(handler, "", 8001):
+    #    await asyncio.Future()  # run forever
+
+    #heroku loop:
+    # Set the stop condition when receiving SIGTERM.
+    loop = asyncio.get_running_loop()
+    stop = loop.create_future()
+    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+
+    port = int(os.environ.get("PORT", "8001"))
+    async with websockets.serve(handler, "", port):
+        await stop
 
 
 if __name__ == "__main__":
